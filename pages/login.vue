151904eff15b5c52481be5ad716e76bef837f644
illia.serbin <illia.serbin@tangramltd.com>
2022-07-23 12:02:57 +0300

    added login page layout plus added auth plugin

---
+++ pages/login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app" class="login">
+    <h2 class="login__title">
+      Login
+    </h2>
+    <v-text-field label="Username" />
+    <v-text-field label="Password" />
+    <v-btn elevation="0" block class="login__button">
+      Login
+    </v-btn>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LoginPage',
+  layout: 'auth'
+}
+</script>
+
+<style lang="scss">
+.login {
+  &__title {
+    font-weight: normal;
+  }
+
+  &__button {
+  }
+
+}
+</style>

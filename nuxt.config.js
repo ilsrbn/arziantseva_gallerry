5d078d951211eeb127413860b7ba89677501b753
illia.serbin <illia.serbin@tangramltd.com>
2023-03-21 22:07:33 +0200

    changed port

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,46 +1,49 @@
-import colors from 'vuetify/es5/util/colors'
-import * as dotenv from 'dotenv'
+import colors from "vuetify/es5/util/colors";
+import * as dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 export default {
-  target: 'static',
-  mode: 'spa',
+  target: "static",
+  mode: "spa",
   server: {
-    host: '0.0.0.0'
+    port: "6789",
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: 'ramzes gallery',
-    title: 'ramzes gallery',
+    titleTemplate: "ramzes gallery",
+    title: "ramzes gallery",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-      { href: 'https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap', rel: 'stylesheet' }
-    ]
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "preconnect", href: "https://fonts.googleapis.com" },
+      {
+        href: "https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap",
+        rel: "stylesheet",
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/variables.scss',
-    '~/assets/hamburgers.css',
-    '~/assets/remix/remixicon.css'
+    "~/assets/variables.scss",
+    "~/assets/hamburgers.css",
+    "~/assets/remix/remixicon.css",
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/masonry', ssr: false },
-    { src: '~/plugins/vue-select', ssr: false },
-    { src: '~/plugins/swiper' }
+    { src: "~/plugins/masonry", ssr: false },
+    { src: "~/plugins/vue-select", ssr: false },
+    { src: "~/plugins/swiper" },
   ],
   loading: {
-    color: 'white',
-    height: '2px'
+    color: "white",
+    height: "2px",
   },
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -49,44 +52,42 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module",
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
-    '@nuxtjs/moment'
+    "@nuxtjs/vuetify",
+    "@nuxtjs/moment",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-    '@nuxtjs/auth-next',
-    '@nuxtjs/toast',
-    'cookie-universal-nuxt',
-    '@nuxtjs/dotenv'
+    "@nuxtjs/pwa",
+    "@nuxtjs/auth-next",
+    "@nuxtjs/toast",
+    "cookie-universal-nuxt",
+    "@nuxtjs/dotenv",
   ],
   auth: {
     strategies: {
       laravelJWT: {
-        provider: 'laravel/jwt',
-        url: '/auth',
+        provider: "laravel/jwt",
+        url: "/auth",
         endpoints: {
-
-          login: { url: '/login', method: 'post', propertyName: false },
-          refresh: { url: '/refresh', method: 'post', propertyName: false },
-          user: { url: '/me', method: 'post', propertyName: false },
-          logout: { url: '/logout', method: 'post', propertyName: false }
-
+          login: { url: "/login", method: "post", propertyName: false },
+          refresh: { url: "/refresh", method: "post", propertyName: false },
+          user: { url: "/me", method: "post", propertyName: false },
+          logout: { url: "/logout", method: "post", propertyName: false },
         },
         token: {
-          property: 'token',
-          maxAge: 60 * 60
+          property: "token",
+          maxAge: 60 * 60,
         },
         refreshToken: {
-          maxAge: 20160 * 60
-        }
-      }
+          maxAge: 20160 * 60,
+        },
+      },
       // local: {
       //   scheme: 'refresh',
       //   token: {
@@ -112,26 +113,26 @@ export default {
       //   }
       //   // autoLogout: false
       // }
-    }
+    },
   },
   toast: {
-    position: 'top-center',
+    position: "top-center",
     duration: 2000,
-    theme: 'bubble',
-    keepOnHover: true
+    theme: "bubble",
+    keepOnHover: true,
   },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.API_URL || 'https://back.ramzes.serbin.co/api'
+    baseURL: process.env.API_URL || "https://back.ramzes.serbin.co/api",
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
-    }
+      lang: "en",
+    },
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
@@ -146,13 +147,12 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
